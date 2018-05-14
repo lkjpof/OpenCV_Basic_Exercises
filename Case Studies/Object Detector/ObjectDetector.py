6aa22cf22b9ea4112308d282583dcf7e31cfafef
Akshay Chandra L <akshaychandra111@gmail.com>
2018-05-14 16:22:18 +0530

    Added 'Object Detection' Case Study

---
+++ Case Studies/Object Detector/ObjectDetector.py
@@ -0,0 +1,61 @@
+import numpy as np
+import time
+import cv2
+
+# Define the upper and lower boundaries for a color to be considered "Blue"
+blueLower = np.array([90, 50, 50])
+blueUpper = np.array([130, 255, 255])
+kernel = np.ones((5, 5), np.uint8)
+
+# Load the video
+camera = cv2.VideoCapture(0)
+
+# Keep looping
+while True:
+    # Grab the current frame
+    (grabbed, frame) = camera.read()
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Check to see if we have reached the end of the video
+    if not grabbed:
+        break
+
+    # Determine which pixels fall within the blue boundaries and then blur the binary image
+    blueMask = cv2.inRange(hsv, blueLower, blueUpper)
+    blueMask = cv2.erode(blueMask, kernel, iterations=2)
+    blueMask = cv2.morphologyEx(blueMask, cv2.MORPH_OPEN, kernel)
+    blueMask = cv2.dilate(blueMask, kernel, iterations=1)
+
+    # find contours in the image
+    (_, cnts, _) = cv2.findContours(blueMask.copy(), cv2.RETR_EXTERNAL,
+    	cv2.CHAIN_APPROX_SIMPLE)
+    center = None
+
+    # Check to see if any contours were found
+    if len(cnts) > 0:
+    	# sort the contours and find the largest one -- we
+    	# will assume this contour correspondes to the area of the bottle cap
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+
+	# show the frame and the binary image
+    cv2.imshow("Tracking", frame)
+    cv2.imshow("Binary", blueMask)
+
+	# if your machine is fast, it may display the frames in
+	# what appears to be 'fast forward' since more than 32
+	# frames per second are being displayed -- a simple hack
+	# is just to sleep for a tiny bit in between frames;
+	# however, if your computer is slow, you probably want to
+	# comment out this line
+    time.sleep(0.025)
+
+	# if the 'q' key is pressed, stop the loop
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
