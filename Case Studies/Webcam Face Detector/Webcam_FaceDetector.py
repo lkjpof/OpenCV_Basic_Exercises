f64a738da143b3e03ffd59d84a880d3fb39fd79b
Akshay Chandra L <akshaychandra111@gmail.com>
2018-05-14 12:46:40 +0530

    Modified Webcam_FaceDetetcor.py (Added comments)

--- Case Studies/Webcam Face Detector/Webcam_FaceDetector.py
+++ Case Studies/Webcam Face Detector/Webcam_FaceDetector.py
@@ -1,24 +1,31 @@
 import numpy as np
 import cv2
 
+# Get the necessary harrcascade classifiers (Face and Eyes)
 face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
-#img = cv.imread('sachin.jpg')
-#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+# Initialize Camera
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    # Convert to gray for ease of computation
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+    # Find the Face in a Gray image for ease of computation
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
+        # Draw the rectange for the face
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+
+        # Find the eyes in a Gray image for ease of computation
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        # Draw the rectangles for the eyes
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
